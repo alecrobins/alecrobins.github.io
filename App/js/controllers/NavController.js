15d60b33d849f49165dc897b7108c1975bd60aed
Alec Robins <alecrobins@gmail.com>
2014-12-17 22:47:24 -0500

    Home About Transition added

--- App/js/controllers/NavController.js
+++ App/js/controllers/NavController.js
@@ -3,10 +3,18 @@
 var navController = angular.module("app").controller('NavController', ['$scope', '$location',
 function ($scope, $location) {
 
+      // used to determine if the user is on/off the home page
+      $scope.isHome = true;
+
 	//nav links
       $scope.gotoProtfolio = function () {
-					//console.log("Hi Abby!!!!");
+          $scope.isHome = false;
           $location.path("/protfolio");
+      };
+
+      $scope.gotoHome = function () {
+          $scope.isHome = true;
+          $location.path("/");
       };
 
 }]);
